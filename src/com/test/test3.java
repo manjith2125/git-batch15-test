a8be20065672ec2dac940a3e00c2fc2af13b5c4f
manjth2125 <manjith2125@gmail>
2026-02-23 12:29:27 +0530

    added test3 file

--- src/com/test/test3.java
+++ src/com/test/test3.java
@@ -1,9 +1,9 @@
 package com.test;
 
-public class Test1 {
-	
+public class test3 {
 	public static void main(String[] args) {
-		System.out.println("Test1 class... VSM");
 		
+		 System.out.println("Test 3...Manjith");
 	}
+
 }
